e8428629a8760d48c61cb6e0fa80c83aa2c15a04
Garyzhai <50354981+niveousswan@users.noreply.github.com>
2026-05-12 22:52:21 +1000

    Delete w09tut/hmacexample1.py

--- w09tut/hmacexample1.py
+++
@@ -1,8 +0,0 @@
-# Example of HMAC
-from cryptography.hazmat.primitives import hashes, hmac
-key = b"This is my key"			# Secret key
-mac = hmac.HMAC(key, hashes.SHA256())	# Select hash algorithm and supply key
-mac.update(b"Steven")			# Apply MAC on message
-t1 = mac.finalize()			# Output the tag
-t1_hex = t1.hex()			# Convert bytes to hex
-print(t1_hex)
